660629e31b19d2396d80e62749fd4c1f6820479f
eletroswing <ytalosilvabatalha@gmail.com>
2023-12-16 13:30:13 -0300

    feat(command): implement compatibility with default npm

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Common is a tool that helps you deal with multiple node_modules directories, avoiding the black hole of super heavy files in every project you create.
 
 # How It Works
-Common works like an npm shell. So, you use the commands `common npm install`, and `common npm uninstall` to install or not a lib taking into account your global environment.
+Common works like an npm shell. So, you use the commands `common npm install`, `common npm uninstall` and `common npm install` to install or not a lib  into your global environment.
 
 # How to use    
 As soon as you run the installation command, a node_modules folder will be found in your project, and you can import your project from that folder.
@@ -10,7 +10,7 @@ Example:
 By default, you run `npm install uuid`, and import with `require('uuid')`;
 Now you run `common npm install uuid` and use `require(uuid')` normally;
 
-This will provide support on your machine, but remember, before running your project on another machine, run common build, which will install the dependencies in your local directory and modify common, so that the libs are imported from the project and not from the machine.
+By default, common does not affect npm packages, so in a project you can use common or npm, this way a repository does not need to be modified to accept common, just as a client does not need to modify it to use npm
 
 # How to build
 Install python requirements then run `pyinstaller common.py`
--- common.py
+++ common.py
@@ -36,8 +36,8 @@ def remove_dependency_from_package_json(package_name, save_dev, script_directory
             print(f"The dependencie '{package_name}' is not present on package.json.")
 
 
-def add_dependency_to_package_json(package_name, save_dev, script_directory):
-    package_json_path = os.path.join(script_directory, 'package.json')
+def add_dependency_to_package_json(package_name, save_dev, dir):
+    package_json_path = os.path.join(dir, 'package.json')
     if(os.path.exists(package_json_path)):
         with open(package_json_path, 'r') as package_json_file:
             package_data = json.load(package_json_file)
@@ -56,7 +56,8 @@ def add_dependency_to_package_json(package_name, save_dev, script_directory):
             print(f"Dependencie '{package_name}' added.")
         else:
             print(f"The dependencie'{package_name}' already present")
-
+    else:
+        print('packge doest not exists', package_json_path)
 def createDirectory(path):
     if not os.path.exists(path):
         try:
@@ -72,14 +73,12 @@ current_working_directory = os.getcwd()
 if(len(arguments) >= 2):
     if arguments[1] == 'npm':
         if arguments[2] == 'install':
-            if(arguments[3]):
+            if(len(arguments) >= 4):
                 package_name = arguments[3]
                 save_dev = '--save-dev' in arguments
 
-                save_dev = '--save-dev' in arguments
-
                 subprocess.run('npm install {} --prefix {}/libs'.format(package_name, script_directory), shell=True, capture_output=True, text=True)
-                add_dependency_to_package_json(package_name, save_dev, script_directory)
+                add_dependency_to_package_json(package_name, save_dev, current_working_directory)
 
                 common_directory = os.path.join(current_working_directory, 'node_modules')
                 createDirectory(common_directory)
@@ -96,21 +95,40 @@ module.exports = {};
             else:
                 package_json_path = os.path.join(script_directory, 'libs', 'package.json')
                 if(os.path.exists(package_json_path)):
+
+                    subprocess.run('npm install --prefix {}/libs'.format(script_directory), shell=True, capture_output=True, text=True)
+
                     with open(package_json_path, 'r') as package_json_file:
                         package_data = json.load(package_json_file)
 
                     dependencies = package_data.get('dependencies', {}).items()
                     dev_dependencies = package_data.get('devDependencies', {}).items()
 
-                    for package_name, version in dependencies:
-                        save_dev = '--save-dev' in arguments
+                    common_directory = os.path.join(current_working_directory, 'node_modules')
+                    createDirectory(common_directory)
 
-                        subprocess.run('npm install {} --prefix {}/libs'.format(package_name, script_directory), shell=True, capture_output=True, text=True)
+                    if bool(dependencies):
+                        for package_name, version in dependencies:
+                            js_content = """const {} = require('{}');
 
-                    for package_name, version in dev_dependencies:
-                        save_dev = '--save-dev' in arguments
+    module.exports = {};
+                    """.format(package_name, script_directory.replace("\\", "/") + "/libs/node_modules/" + package_name, package_name)
 
-                        subprocess.run('npm install {} --prefix {}/libs'.format(package_name, script_directory), shell=True, capture_output=True, text=True)
+                            js_path = os.path.join(common_directory, "{}.js".format(package_name))
+                            with open(js_path, "w+") as archive:
+                                archive.write(js_content)
+
+                    if bool(dev_dependencies):
+                        for package_name, version in dev_dependencies:
+                            js_content = """const {} = require('{}');
+
+module.exports = {};
+                """.format(package_name, script_directory.replace("\\", "/") + "/libs/node_modules/" + package_name, package_name)
+
+                        js_path = os.path.join(common_directory, "{}.js".format(package_name))
+                        with open(js_path, "w+") as archive:
+                            archive.write(js_content)
+
 
         elif arguments[2] == 'uninstall':
             if(arguments[3]):
